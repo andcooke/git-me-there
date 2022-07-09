e34e8c31d39707cedd538a104450d0e1ff08811e
King David <103971670+kyle-david1@users.noreply.github.com>
2022-07-09 14:09:21 -0500

    two FUNCTIONING api calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,11 @@
 // Use at least two [server-side APIs](https://coding-boot-camp.github.io/full-stack/apis/api-resources).
-var wsApiKey = '8d073cbbe64bd87c6d0e21ed06c0c5b3';
-var cityApiKey = 'd2e2c17de561fb5216c9679df62394b5';
-var scoreUrl = 'https://api.walkscore.com/score';
-var cityName = 'http://api.openweathermap.org/geo/1.0/direct?q=${userSearch}&appid=${d2e2c17de561fb5216c9679df62394b5}';
+// var wsApiKey = '8d073cbbe64bd87c6d0e21ed06c0c5b3';
+// var cityApiKey = `d2e2c17de561fb5216c9679df62394b5`;
+// var scoreUrl = 'https://api.walkscore.com/score';
+// var cityName = 'http://api.openweathermap.org/geo/1.0/direct?q=${userSearch}&appid=${d2e2c17de561fb5216c9679df62394b5}';
 var userSearch = ""
-// var lat = 30;
-// var lon = -97;
-var userSearch = 'Austin';
-
+var userSearch = prompt("Where do you want to go")
+// variables for all the things im searching for
 
 // 'https://api.walkscore.com/score?format=json&
 // address=${address}&lat=${lat}&
@@ -17,26 +15,38 @@ function getLatLon(){
 // function getLatLon then pass into geoAPI -------does 30 work for limit number? what should this be?
 
 // function and fetch to get lat and lon and pass into function for walk score
-fetch('http://api.openweathermap.org/geo/1.0/direct?q=${userSearch}&appid=${d2e2c17de561fb5216c9679df62394b5}')    
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+ userSearch + '&appid=d2e2c17de561fb5216c9679df62394b5')    
     .then(function(response){
         return response.json();
-    }).then(function(data){
+    })
+    .then(function(data){
         console.log(data);
-        console.log(lat, data[0].lat)
-        console.log(lon, data[0].lon)
-        lat = data[0].lat
-        lon= data[0].lon
-        getLatLon();
-    });
-    };
-
-    // fetch request for walk score 
-var walkScore = ('https://salty-mountain-68764.herokuapp.com/api.walkscore.com/score?format=json&address=1119%8th%20Avenue%20Seattle%20WA%2098101&lat=47.6085&lon=-122.3295&transit=1&bike=1&wsapikey=8d073cbbe64bd87c6d0e21ed06c0c5b3') 
+        var lat = data.coord.lat
+        var lon = data.coord.lon
+        console.log("this works", lat)
+        console.log("this deux works", lon)
+        
     
-fetch(walkScore)
+    fetch('https://salty-mountain-68764.herokuapp.com/api.walkscore.com/score?format=json&address=1119%8th%20Avenue%20Seattle%20WA%2098101&lat=' + lat + '&lon=' + lon + '&transit=1&bike=1&wsapikey=8d073cbbe64bd87c6d0e21ed06c0c5b3')
 .then(function(response){
     return response.json();
+
+})
+.then(function(data){
+    console.log(data);
+})
 });
+    };
+    
+    getLatLon()
+    // fetch request for walk score 
+
+    
+
+
+
+
+
 
     // function getWalkScore($lat, $lon, $address) {
     //  $address=urlencode($address);
