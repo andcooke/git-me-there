5325805cd5afb40add220b67a869dc0993c43e38
King David <103971670+kyle-david1@users.noreply.github.com>
2022-07-11 14:51:30 -0500

    additions to CS, HTML, index.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,17 @@
+var walkScoreEl = document.getElementById('walking-score');
+var bikeScoreEl= document.getElementById('biking-score');
+var walkDescription = document.getElementById('walk-description');
+var bikeDescription = document.getElementById('bike-description');
 
 
+function getParams (){
+   var displayScore = document.location.search.split('=').pop();
+   getLatLon(displayScore)
+}
 
-var userSearch = prompt("Where do you want to go");
-// variables for all the things im searching for
-var searchinputEl = document.getElementById('search-input');
-// var address = searchinputEl.value
 
+function getLatLon(userSearch){
 
-function getLatLon(){
-
-    // let userSearch = document.querySelector(".btn");
-    // console.log(userSearch, "search fired properly");
-
-// function and fetch to get lat and lon and pass into function for walk score
 fetch('https://api.openweathermap.org/data/2.5/weather?q='+ userSearch + '&appid=d2e2c17de561fb5216c9679df62394b5')    
     .then(function(response){
         return response.json();
@@ -25,18 +24,27 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q='+ userSearch + '&appid
         console.log("this deux works", lon)
         
     
-    fetch('https://salty-mountain-68764.herokuapp.com/api.walkscore.com/score?format=json&address='+ userSearch +'&lat=' + lat + '&lon=' + lon + '&transit=1&bike=1&wsapikey=8d073cbbe64bd87c6d0e21ed06c0c5b3')
-.then(function(response){
-    return response.json();
+        fetch('https://salty-mountain-68764.herokuapp.com/api.walkscore.com/score?format=json&address='+ userSearch +'&lat=' + lat + '&lon=' + lon + '&transit=1&bike=1&wsapikey=8d073cbbe64bd87c6d0e21ed06c0c5b3')
+        .then(function(response){
+            return response.json();
 
-})
-.then(function(data){
-    console.log(data);
-})
-});
-    };
+
+        })
+        .then(function(data){
+            console.log(data);
+            console.log(data.bike.score);
+            bikeScoreEl.textContent = data.bike.score;
+            bikeDescription.textContent = data.bike.description;
+            console.log(data.walkscore);
+            walkScoreEl.textContent = data.walkscore;
+            walkDescription.textContent = data.description;
+        })
+        });
+    }
     
-    getLatLon()
+
+  
+    getParams();
 
     // does query selector go inside or outside the function, before or after
     // let userSearch = document.querySelector(".btn");
@@ -44,15 +52,11 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q='+ userSearch + '&appid
 
 
     // add event listener to start button 
-    addEventListener
+   
+
 
     // appending different parameters to the object 
-    
 
-
-
-
-
-
-
-    // Use client-side storage to store persistent data
+    // var bikeDescription = document.createElement('span')
+    // bikeDescription.textContent = data.bike.description;
+    // bikeScoreEl.appendChild(bikeDescription)
