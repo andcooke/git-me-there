a50c11e86a249b993650ce623b1bdf854471ffef
A <andcooke@duck.com>
2022-07-10 21:04:55 -0500

    linked js between htmls with queryString

---
+++ assets/js/search.js
@@ -0,0 +1,18 @@
+const searchButtonEl = document.querySelector("#search-button");
+
+const handleSearchSubmit = (event) => {
+    event.preventDefault();
+
+    let searchInputVal = document.querySelector("#search-input").value;
+
+    if (!searchInputVal) {
+        console.error("You need a search input!");
+        return;
+    }
+
+    let queryString = "./results.html?q=" + searchInputVal;
+    
+    location.assign(queryString);
+};
+
+searchButtonEl.addEventListener("click", handleSearchSubmit);
